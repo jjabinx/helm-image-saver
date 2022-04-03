e94a3c4562c52c2cb4cc8e18d197df4fa9d78a4e
JJ <75303112+jjabinx@users.noreply.github.com>
2022-04-03 16:39:44 +0300

    Update save-images script

--- save-images.sh
+++ save-images.sh
@@ -1,10 +1,9 @@
 CHART_PATH=$1
 CHART_NAME=$2
-TARGET_PATH=$3
 
 IMAGES=`helm template "$CHART_NAME" "$CHART_PATH" --set certmanager-issuer.email=a@a.a | grep image: | sed "s/image: //g" | sed 's/"//g' | sort --unique`
 for image in $IMAGES; do 
-    # repo=`echo $image | cut -d ":" -f1`
+    repo=`echo $image | cut -d ":" -f1`
     # tag=`echo $image | cut -d ":" -f2 | cut -d "@" -f1`
     # hash=""
     # if echo $image | grep '@'; 
@@ -13,7 +12,8 @@ for image in $IMAGES; do
     # else 
     #     echo $image
     # fi 
-    docker pull $image
+    echo "Pulling $repo"
+    docker pull $image > /dev/null
 done
 
-docker save $(sudo docker images | awk '/a/ {print $1 ":" $2}') -o $TARGET_PATH
+docker save $IMAGES -o ./$CHART_NAME.tar
